f31ee3154546ec0b32d1402f52892e9564f51f85
Ashish Deshmukh <84738585+Ashish-Deshmukh007@users.noreply.github.com>
2024-07-27 18:21:38 +0530

    Update input.java

--- Basic_Java/input.java
+++ Basic_Java/input.java
@@ -2,7 +2,16 @@ public class JavaBasics{
   public static void main(String args[]){
     //code here
     Scanner sc= new Scanner(System.in);
-    String input= sc.next;
+    String input= sc.next();     //sc.next method is used to input String data type
+    /*nextLine() -> Full Line of Strings
+    nextInt() -> For integer data type
+    nextByte() -> For Byte data type
+    nextFloat() -> For Float data type
+    nextDouble() -> For Double data type
+    nextBoolean() -> For Boolean data type
+    nextShort() ->  For Short data type
+    nextLong() -> For Long data type
+      */
     System.out.print(input);
   }
 }
