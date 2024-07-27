af83ee941d4500fd373610c7f026079d82f433db
Ashish Deshmukh <84738585+Ashish-Deshmukh007@users.noreply.github.com>
2024-07-27 18:27:36 +0530

    Create Solution.java

---
+++ Variables and Data Types/Q1/Solution.java
@@ -0,0 +1,11 @@
+public class Solution{
+  public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter 3 numbers:");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    int average=(a+b+c)/3;
+    System.out.println("Average is:"+average);
+  }
+}
