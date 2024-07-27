fce51f5e1840368b632e06086249138a2d005368
Ashish Deshmukh <84738585+Ashish-Deshmukh007@users.noreply.github.com>
2024-07-27 18:17:11 +0530

    Create input.java
    This code shows how to input in java

---
+++ Basic_Java/input.java
@@ -0,0 +1,8 @@
+public class JavaBasics{
+  public static void main(String args[]){
+    //code here
+    Scanner sc= new Scanner(System.in);
+    String input= sc.next;
+    System.out.print(input);
+  }
+}
