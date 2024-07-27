8d38b3b8b47a3c1103f1a2c49d98cd79f4760c47
Ashish Deshmukh <84738585+Ashish-Deshmukh007@users.noreply.github.com>
2024-07-27 18:15:40 +0530

    Create output.java
    This code shows how to give output in java

---
+++ Basic_Java/output.java
@@ -0,0 +1,7 @@
+public class JavaBasics{
+  public static void main(String args[]){
+    //code here
+    System.out.print("Hello World");
+    System.out.println("Hello World");   // cursor ends up on the next line
+  }
+}
