b8bb8603e42efee94c2b3f3952ef5f285468b783
Ashish Deshmukh <84738585+Ashish-Deshmukh007@users.noreply.github.com>
2024-07-27 18:31:07 +0530

    Create Solution.java

---
+++ Variables and Data Types/Q2/Solution.java
@@ -0,0 +1,9 @@
+public class Solution{
+  public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter the side of the square:");
+    int side=sc.nectInt();
+    int area=side*side;
+    System.out.println("The area of the square is:"+area);
+  }
+}
